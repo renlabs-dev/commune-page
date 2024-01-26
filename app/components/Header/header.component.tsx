f032dc6bb542e87e19228be366c571abb03ed44e
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-01-26 23:08:45 +1100

    feature: new header style

--- app/components/Header/header.component.tsx
+++ app/components/Header/header.component.tsx
@@ -1,36 +1,38 @@
 'use client'
+
 import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const navigation = [
-  { name: 'Commune AI', href: '#' },
+  { name: 'Home', href: '#' },
   { name: 'Docs', href: '#' },
   { name: 'Whitepaper', href: '#' },
   { name: 'Modules', href: '#' },
-  { name: 'Telemetry', href: '#' },
-  { name: 'Exchanges', href: '#' },
 ]
 
 export const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className=' top-0 z-50 w-full bg-gray-800/30'>
+    <header className='fixed z-50 w-full'>
       <nav
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
+        className='mx-auto flex w-full items-center justify-between bg-gray-900/10 p-6 backdrop-blur-sm lg:px-[5%]'
         aria-label='Global'
       >
-        <a href='#' className='-m-1.5 p-1.5'>
+        <a
+          href='#'
+          className={`-m-1.5 p-1.5 ${mobileMenuOpen && 'opacity-0'} flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10`}
+        >
           <span className='sr-only'>Commune AI</span>
           <Image
-            src={'./commune-v1.svg'}
-            width={61.67}
-            height={90}
+            src={'./commune-v2.svg'}
+            width={32}
+            height={32}
             alt='commune logo'
-            className='h-12'
+            id='teste'
+            className='mr-[3px] h-8'
           />
         </a>
         <div className='flex lg:hidden'>
@@ -55,28 +57,25 @@ export const Header = () => {
           ))}
         </div>
       </nav>
-      <Dialog
-        as='div'
-        className='lg:hidden'
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
+
+      <div
+        className={`lg:hidden ${mobileMenuOpen ? 'visible' : 'hidden'}`}
+        onClick={() => setMobileMenuOpen(false)}
       >
         <div className='fixed inset-0 z-10' />
-        <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900/90 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+        <div className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900/50 px-6 py-6 backdrop-blur-sm sm:ring-1 sm:ring-white/10'>
           <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
-              {/* <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              /> */}
+            <a
+              href='#'
+              className='-m-1.5 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 p-1.5'
+            >
+              <span className='sr-only'>Commune</span>
               <Image
-                src={'./commune-v1.svg'}
-                width={61.67}
-                height={90}
+                src={'./commune-v2.svg'}
+                width={32}
+                height={32}
                 alt='commune logo'
-                className='h-12'
+                className='mr-[3px] h-8'
               />
             </a>
             <button
@@ -90,29 +89,21 @@ export const Header = () => {
           </div>
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-400/10'>
-              <div className='space-y-2 py-6'>
+              <div className='mt-10 space-y-2 py-6'>
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-800'
+                    className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-900/10 hover:backdrop-blur-sm'
                   >
                     {item.name}
                   </a>
                 ))}
               </div>
-              {/* <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-800"
-                >
-                  Log in
-                </a>
-              </div> */}
             </div>
           </div>
-        </Dialog.Panel>
-      </Dialog>
+        </div>
+      </div>
     </header>
   )
 }
