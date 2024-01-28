472c78104634c46b54e292040543aef676910652
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-01-28 13:51:00 +1100

    feature: reorganize header options and improve style

--- app/components/Header/header.component.tsx
+++ app/components/Header/header.component.tsx
@@ -6,10 +6,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const navigation = [
-  { name: 'Home', href: '#' },
-  { name: 'Docs', href: '#' },
-  { name: 'Whitepaper', href: '#' },
-  { name: 'Modules', href: '#' },
+  { name: 'Docs', href: '#', external: false },
+  { name: 'Whitepaper', href: 'https://ai-secure.github.io/DMLW2022/assets/papers/7.pdf', external: true },
+  { name: 'Modules', href: '#', external: false },
 ]
 
 export const Header = () => {
@@ -18,12 +17,12 @@ export const Header = () => {
   return (
     <header className='fixed z-50 w-full'>
       <nav
-        className='mx-auto flex w-full items-center justify-between bg-gray-900/10 p-6 backdrop-blur-sm lg:px-[5%]'
+        className={`mx-auto ${mobileMenuOpen && 'hidden'} grid grid-cols-3 grid-flow-col w-full items-center justify-between bg-gray-900/10 p-6 backdrop-blur-sm lg:px-[5%]`}
         aria-label='Global'
       >
-        <a
+        <Link
           href='#'
-          className={`-m-1.5 p-1.5 ${mobileMenuOpen && 'opacity-0'} flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10`}
+          className={`p-1.5 col-span-1 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]`}
         >
           <span className='sr-only'>Commune AI</span>
           <Image
@@ -31,43 +30,75 @@ export const Header = () => {
             width={32}
             height={32}
             alt='commune logo'
-            id='teste'
             className='mr-[3px] h-8'
+            priority
           />
-        </a>
-        <div className='flex lg:hidden'>
+        </Link>
+        <div className='hidden lg:flex lg:gap-x-12 justify-center'>
+          {navigation.map((item) => (
+            <Link
+              target={item.external ? '_blank' : '_self'}
+              key={item.name}
+              href={item.href}
+              className='text-sm font-semibold leading-6 text-gray-100 hover:text-gray-400'
+            >
+              {item.name}
+            </ Link>
+          ))}
+        </div>
+        <div className='hidden lg:flex lg:gap-x-4 justify-end'>
+          <Link
+            href='https://github.com/commune-ai'
+            target='_blank'
+            className={`p-1.5 ${mobileMenuOpen && 'opacity-0'} flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]`}
+          >
+            <span className='sr-only'>Commune Github</span>
+            <Image
+              priority
+              src={'./github-icon-white.svg'}
+              width={32}
+              height={32}
+              alt="Github logo"
+            />
+          </Link>
+          <Link
+            href='https://github.com/commune-ai'
+            target='_blank'
+            className={`p-1.5 ${mobileMenuOpen && 'opacity-0'} flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]`}
+          >
+            <span className='sr-only'>Commune Discord</span>
+            <Image
+              priority
+              src={'./discord-icon-white.svg'}
+              width={32}
+              height={32}
+              alt="Discord logo"
+            />
+          </Link>
+        </div>
+        <div className='flex lg:hidden col-span-3 justify-end'>
           <button
             type='button'
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-100'
+            className='-m-2.5 items-center justify-center p-1.5 text-gray-100 flex h-12 w-12 rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]
+            '
+
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className='sr-only'>Open main menu</span>
             <Bars3Icon className='h-6 w-6' aria-hidden='true' />
           </button>
         </div>
-        <div className='hidden lg:flex lg:gap-x-12'>
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className='text-sm font-semibold leading-6 text-gray-100 hover:text-gray-400'
-            >
-              {item.name}
-            </Link>
-          ))}
-        </div>
       </nav>
 
-      <div
+      <nav
         className={`lg:hidden ${mobileMenuOpen ? 'visible' : 'hidden'}`}
-        onClick={() => setMobileMenuOpen(false)}
       >
         <div className='fixed inset-0 z-10' />
         <div className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900/50 px-6 py-6 backdrop-blur-sm sm:ring-1 sm:ring-white/10'>
           <div className='flex items-center justify-between'>
-            <a
+            <Link
               href='#'
-              className='-m-1.5 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 p-1.5'
+              className="p-1.5 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]"
             >
               <span className='sr-only'>Commune</span>
               <Image
@@ -77,10 +108,10 @@ export const Header = () => {
                 alt='commune logo'
                 className='mr-[3px] h-8'
               />
-            </a>
+            </Link>
             <button
               type='button'
-              className='-m-2.5 rounded-md p-2.5 text-gray-100'
+              className='-m-2.5 items-center justify-center p-1.5 text-gray-100 flex h-12 w-12 rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]'
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className='sr-only'>Close menu</span>
@@ -88,22 +119,51 @@ export const Header = () => {
             </button>
           </div>
           <div className='mt-6 flow-root'>
-            <div className='-my-6 divide-y divide-gray-400/10'>
-              <div className='mt-10 space-y-2 py-6'>
+            <div className='-my-6 divide-y divide-gray-400/20'>
+              <div className='mt-10 space-y-2 py-6 ml-2'>
                 {navigation.map((item) => (
-                  <a
+                  <Link
+                    target={item.external ? '_blank' : '_self'}
                     key={item.name}
                     href={item.href}
-                    className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-900/10 hover:backdrop-blur-sm'
+                    className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-400/10 hover:backdrop-blur-sm'
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
+              </div>
+              <div className="py-6">
+                <Link
+                  href='https://github.com/commune-ai'
+                  target='_blank'
+                  className={`p-1.5 mb-4 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]`}
+                >
+                  <span className='sr-only'>Commune Github</span>
+                  <Image
+                    src={'./github-icon-white.svg'}
+                    width={32}
+                    height={32}
+                    alt="Github logo"
+                  />
+                </Link>
+                <Link
+                  href='https://github.com/commune-ai'
+                  target='_blank'
+                  className="p-1.5 flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 hover:bg-gray-100/[0.15]"
+                >
+                  <span className='sr-only'>Commune Discord</span>
+                  <Image
+                    src={'./discord-icon-white.svg'}
+                    width={32}
+                    height={32}
+                    alt="Discord logo"
+                  />
+                </Link>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </nav>
     </header>
   )
 }
