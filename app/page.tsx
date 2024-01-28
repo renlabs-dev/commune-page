e06f22b757686d100b25f7b3e9ee70d57c098650
Vinícius Porto do Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-01-28 08:49:54 -0300

    feature: implement suspense loading (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
+import { Suspense } from 'react'
 import { GenericSection, Header, HeroSection } from './components'
 import { CallToActionSection } from './components/Sections/CallToAction'
 import { sections } from './utils'
+import Loading from './loading'
 
 export default async function Home() {
   return (
     <>
       <Header />
       <HeroSection />
-      <div className='mx-8 flex flex-col items-center'>
-        <CallToActionSection />
-        {sections.map((section, index) => {
-          return (
-            <GenericSection
-              key={index}
-              sectionName={section.sectionName}
-              title={section.title}
-              subtitle={section.subtitle}
-              color={section.color}
-              features={section.features}
-              gradientColor={section.gradientColor}
-            />
-          )
-        })}
-      </div>
+      <Suspense fallback={<Loading />} >
+        <div className='mx-8 flex flex-col items-center'>
+          <CallToActionSection />
+          {sections.map((section, index) => {
+            return (
+              <GenericSection
+                key={index}
+                sectionName={section.sectionName}
+                title={section.title}
+                subtitle={section.subtitle}
+                color={section.color}
+                features={section.features}
+                gradientColor={section.gradientColor}
+              />
+            )
+          })}
+        </div>
+      </Suspense>
     </>
   )
 }
