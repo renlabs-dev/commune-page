7494734c7320ed679b1724ecfa1a5d7abd1c1186
Vinícius Porto do Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-01-29 05:10:00 -0300

    feature: refactor components and enhance layout effects (#15)

--- app/components/BackgroundEffects/gradient-layer.component.tsx
+++ app/components/BackgroundEffects/gradient-layer.component.tsx
@@ -1,38 +1,31 @@
 type TGradientLayerProps = {
-  wrapperOverwriteStyle?: string
-  gradientColor?: string
-  gradientStyle?: string
-  direction?: 'left' | 'right' | 'top' | 'bottom'
-}
+  wrapperOverwriteStyle?: string;
+  gradientColor?: string;
+  gradientStyle?: string;
+};
 
-export const GradientLayer = (props: TGradientLayerProps) => {
-  const {
-    gradientColor = '',
-    gradientStyle = '',
-    wrapperOverwriteStyle = '',
-    direction = 'right',
-  } = props
+export const GradientLayer: React.FC<TGradientLayerProps> = ({
+  gradientColor = '',
+  wrapperOverwriteStyle = '',
+}) => {
+  const gradientClipPath =
+    'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)';
 
   return (
-    <>
+    <div
+      className={`absolute top-10 -z-10 transform-gpu blur-3xl lg:top-[calc(50%-30rem)] ${wrapperOverwriteStyle}`}
+      aria-hidden='true'
+    >
       <div
-        className={`absolute ${direction}-[calc(50%+4rem)] top-10 -z-10 transform-gpu blur-3xl sm:${direction}-[calc(50%+18rem)] lg:${direction}-48 lg:top-[calc(50%-30rem)] xl:${direction}-[calc(50%+12rem)] ${wrapperOverwriteStyle}`}
-        aria-hidden='true'
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
-          }}
-          className={`
+        style={{ clipPath: gradientClipPath }}
+        className={`
           aspect-[1108/632] w-[69.25rem] 
           bg-gradient-to-r
-          opacity-30 
+          opacity-30
           ${gradientColor}
-          ${gradientStyle}
-         `}
-        />
-      </div>
-    </>
-  )
-}
+        `}
+      />
+    </div>
+  );
+};
+
--- app/components/Sections/CallToAction/call-to-action-section.component.tsx
+++ app/components/Sections/CallToAction/call-to-action-section.component.tsx
@@ -1,39 +1,20 @@
-import { DiscordWidget } from '../..'
+import { DiscordWidget, GradientLayer } from '../..'
 
 export const CallToActionSection = () => {
   return (
-    <section
-      id='discord'
-      className={`relative isolate m-6 flex w-11/12 max-w-screen-xl flex-col items-center overflow-hidden rounded-3xl bg-gray-800  p-6 text-center shadow-2xl lg:m-8`}
-    >
-      <h2 className='max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-3xl'>
-        Commune Hub
-      </h2>
-      <p className='mt-6 max-w-xl text-base leading-8 text-gray-300'>
-        Join Commune&apos;s Discord. Code, connect, innovate – Shape the Future
-        Together! 🚀
-      </p>
-      <DiscordWidget />
-      <svg
-        viewBox='0 0 1024 1024'
-        className='absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]'
-        aria-hidden='true'
-      >
-        <circle
-          cx={512}
-          cy={512}
-          r={512}
-          fill='url(#827591b1-ce8c-4110-b064-7cb85a0b1217)'
-          fillOpacity='0.7'
-        />
-        <defs>
-          <radialGradient id='827591b1-ce8c-4110-b064-7cb85a0b1217'>
-            <stop stopColor='#7775D6' />
-            <stop offset={1} stopColor='#E935C1' />
-          </radialGradient>
-        </defs>
-      </svg>
-      {/* </div> */}
+    <section id='discord' className='m-6 flex w-11/12 max-w-screen-xl flex-col items-center overflow-hidden rounded-3xl bg-gray-800/20 p-6 py-12 text-center shadow-xl lg:m-8'>
+      <GradientLayer gradientColor='from-blue-400 via-blue-200' />
+
+      <div className='max-w-2xl'>
+        <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>Commune Hub</h2>
+        <p className='mt-4 text-base leading-7 text-gray-300'>
+          Join Commune&apos;s Discord. Code, connect, innovate – Shape the Future Together! 🚀
+        </p>
+      </div>
+
+      <div className='mt-6'>
+        <DiscordWidget />
+      </div>
     </section>
   )
 }
