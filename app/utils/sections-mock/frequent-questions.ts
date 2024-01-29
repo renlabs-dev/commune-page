b501f6ce3eee440dc5adad2d4f3588f32ae52529
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-01-29 22:38:26 +1100

    feature: create faq and new animations

---
+++ app/utils/sections-mock/frequent-questions.ts
@@ -0,0 +1,49 @@
+export const faqData = [
+  {
+    question: 'What is Commune and its philosophy?',
+    answer:
+      'Commune is a protocol that connects all developer tools into one network, fostering a shareable, reusable, and open economy. It follows an inclusive design philosophy that emphasizes collaboration over isolation, aiming for synergy by enabling developers to leverage Commune alongside their existing projects. This philosophy is maximally unopinionated, granting developers the freedom to incorporate additional tools that they find valuable.',
+  },
+  {
+    question: 'Are there any similar projects?',
+    answer: 'Yes, Commune is a highly adapted fork of Bittensor (TAO).',
+  },
+  {
+    question: 'Who is founder of communeai?',
+    answer:
+      'Sal Vivona, a physicist and machine learning engineer who quit big tech 3 years ago to fulltime focus on Commune. He discovered bittensor and joined the OpenTensor foundation as a developer until he eventually quit to pursue Commune fully again.',
+  },
+  {
+    question: 'How to buy $COM?',
+    answer:
+      'You can buy $COM using USDT or TAO directly without relying on third-party websites. Visit https://comswap.io/ for more information.',
+  },
+  {
+    question: 'How to set up a Wallet or Module?',
+    answer:
+      "To set up a wallet, join our Discord server and navigate to Tutorials > Setup menu for PC. Install Commune and create your wallet directly on your machine. Don't forget to record the mnemonic. Additionally, you can use the web wallet at https://comwallet.io/ (Third-party, use at your own risk). For module setup and deployment, refer to https://commune-t.pages.dev/docs/next/create-commune and https://commune-t.pages.dev/docs/next/deploy-module respectively.",
+  },
+  {
+    question: 'What is the emission schedule for $COM?',
+    answer:
+      'The current emission schedule for $COM is similar to Bitcoin but proportional to 1B total supply and 25% lower inflation rate, with 250k token rewards a day.',
+  },
+  {
+    question: 'What is the total supply of COM tokens?',
+    answer: "It's 1 billion.",
+  },
+  {
+    question: 'What is the current token emission?',
+    answer: "It's 250k tokens/daily.",
+  },
+  {
+    question: 'How can I stake COM tokens?',
+    answer:
+      'You can stake COM tokens either by using COM Wallet (https://comwallet.io/) or by using the Commune framework (https://medium.com/@Calc482/step-by-step-guide-to-delegating-tokens-to-commune-validators-d5a5bd050973).',
+  },
+  {
+    question: 'How can I get started?',
+    answer:
+      'Please refer to our introduction page where you can find all details: https://commune-t.pages.dev/docs/next/Introduction.',
+  },
+]
