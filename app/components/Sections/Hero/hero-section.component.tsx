ae94d04bd349e60de2ef93297595d37fa6fba99f
Vinícius Porto do Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-01-30 00:38:17 -0300

    feature: create a new cta to docs and improve animations and layout (#17)

--- app/components/Sections/Hero/hero-section.component.tsx
+++ app/components/Sections/Hero/hero-section.component.tsx
@@ -11,33 +11,21 @@ export const HeroSection = () => {
       <GradientLayer gradientColor='from-sky-400 to-sky-200' />
 
       <div className='max-w-7xl py-0 lg:px-8 lg:py-40'>
-        <div className='mx-auto flex max-w-2xl flex-shrink-0 flex-col items-center justify-center lg:mx-0 lg:max-w-xl '>
+        <div className='mx-auto flex max-w-2xl flex-shrink-0 animate-fade-slide-down flex-col items-center justify-center lg:mx-0 lg:max-w-xl '>
           <div className='flex items-center'>
-            <h1 className='bg-gradient-to-br from-sky-300 to-white bg-clip-text text-4xl font-bold tracking-tight text-transparent'>
-              Commune AI
+            <h1 className='bg-gradient-to-br from-sky-300 to-white bg-clip-text text-5xl font-bold tracking-tight text-transparent'>
+              commune ai
             </h1>
           </div>
-
-          <p className='mt-6 text-center text-2xl leading-relaxed tracking-tight text-gray-100 lg:text-center'>
-            Renovating the way we build software for everyone.
+          <p className='mt-6 text-center text-xl font-extralight leading-relaxed tracking-tight text-gray-100 lg:text-center'>
+            Renovating the way we build software.
           </p>
           <div className='z-10 mt-10 flex items-center gap-x-6'>
-            <Link href='#discord'>
-              <span
-                className='rounded-3xl bg-gradient-to-br from-indigo-500 to-blue-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-all ease-in hover:from-indigo-500 hover:to-blue-900 hover:ease-in focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-400'
-                aria-label='Get started with Commune AI'
-              >
-                Get started
-              </span>
-            </Link>
             <Link
-              href='#networking'
-              className='text-sm font-semibold leading-6 text-white'
-              aria-label='Learn more about Commune AI'
+              href='#discord'
+              className='font-base rounded-3xl bg-gradient-to-br from-indigo-500 to-blue-800 px-3.5 py-2.5 text-sm text-white shadow-sm hover:animate-scale-up-sm hover:from-indigo-500 hover:to-blue-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-400'
             >
-              <span>
-                Learn more <span aria-hidden='true'>→</span>
-              </span>
+              <span aria-label='Get started with Commune AI'>Start</span>
             </Link>
           </div>
         </div>
