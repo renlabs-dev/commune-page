cf5161405d1621a6bfbb6418f204c67532671536
Vinícius Porto do Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-01-30 01:33:46 -0300

    fix: header overlay (#19)

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
     <html lang='en'>
       <body className={`${inter.className} bg-gray-900`}>
         <Header />
-
         {children}
       </body>
     </html>
