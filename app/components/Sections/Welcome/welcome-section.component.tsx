d481d57a425c0ca167b54891527b82fc90b57e5a
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-03-23 20:21:00 +1100

    feature: responsive hero, welcome and cta

--- app/components/Sections/Welcome/welcome-section.component.tsx
+++ app/components/Sections/Welcome/welcome-section.component.tsx
@@ -7,56 +7,57 @@ export const WelcomeSection = () => {
   return (
     <div
       id='welcome'
-      className='relative isolate flex h-auto w-full flex-col items-center overflow-hidden bg-[#F9EFE4] py-28'
+      className='relative isolate flex h-auto w-full flex-col items-center justify-center bg-[#F9EFE4] p-10 py-12 lg:py-28'
     >
       <Image
         src={'/dotted-background.svg'}
         width={250}
         height={150}
         alt=''
-        className='absolute -left-20 top-[36%] -z-10'
+        className='hidden lg:block absolute -left-20 top-[36%] -z-10'
       />
       <Image
         src={'/dotted-background.svg'}
         width={250}
         height={150}
         alt=''
-        className='absolute bottom-[8%] right-5 -z-10'
+        className='hidden lg:block absolute bottom-[8%] right-5 -z-10'
       />
-      <div className='mb-24 flex flex-col items-center'>
-        <h3 className='text-5xl font-bold text-title'>
+      <div className='flex flex-col items-center mb-6 text-center lg:mb-24'>
+        <h3 className='mb-10 text-2xl font-bold lg:text-5xl text-title lg:mb-0'>
           Welcome to the
           <span
-            className={`${covered_by_your_grace.className} -ml-3 text-6xl font-normal text-[#FF6C6C]`}
+            className={`${covered_by_your_grace.className} -ml-3 text-4xl lg:text-6xl font-normal text-[#FF6C6C]`}
           >
             {' '}
             commune
           </span>
           !
         </h3>
-        <h4 className='text-xl font-medium text-subtitle'>
+        <h4 className='text-base font-medium lg:text-xl text-subtitle'>
           A place for
           <span
-            className={`${covered_by_your_grace.className} text-3xl font-normal text-[#62ABFF]`}
+            className={`${covered_by_your_grace.className} text-lg lg:text-3xl -mx-1 font-normal text-[#62ABFF]`}
           >
             {' '}
-            everyone{' '}
+            everyone
+            {' '}
           </span>
           to develop design and create.
         </h4>
       </div>
 
-      <div className='flex space-x-10'>
+      <div className='flex flex-col justify-center w-full space-y-10 lg:space-x-5 xl:space-x-8 lg:space-y-0 lg:flex-row'>
         {sections.map((section) => {
           return (
             <Link
               href='#'
               key={section.sectionName}
-              className='flex w-full max-w-48 flex-col items-center text-pretty rounded-xl border-2 border-title bg-white px-8 py-5 text-center shadow-custom'
+              className='flex flex-row-reverse items-center justify-end p-5 bg-white border-2 lg:w-1/5 lg:max-w-[200px] lg:flex-col lg:justify-between rounded-xl border-title lg:py-8 shadow-custom'
             >
-              <h5>{section.title}</h5>
-              <span className='my-auto mt-3 flex h-[130px] items-center'>
-                Image
+              <h5 className='w-2/3 text-xl font-medium text-left lg:w-full lg:text-center lg:mb-10'>{section.title}</h5>
+              <span className={`mr-5 lg:mr-0 ${section.color} rounded-full`}>
+                <Image src={section.iconSrc} width={70} height={70} alt={section.sectionName} className='lg:h-[100px] lg:w-[100px]' />
               </span>
             </Link>
           )
--- app/utils/sections-mock/sections-mock.ts
+++ app/utils/sections-mock/sections-mock.ts
@@ -1,4 +1,3 @@
-import { gradientSectionColors } from './gradientSectionColors'
 import { networkingFeatures } from './networking-features'
 import { reusabilityFeatures } from './reusability-features'
 import { scalabilityFeatures } from './scalability-features'
@@ -10,40 +9,40 @@ export const sections = [
     sectionName: 'networking',
     title: 'Networking & Wrapping',
     subtitle: 'Everything you need',
-    color: 'bg-blue-400/[0.03]',
+    color: 'bg-[#62ABFF]',
     features: networkingFeatures,
-    gradientColor: gradientSectionColors.blue,
+    iconSrc: '/networking-icon.svg',
   },
   {
     sectionName: 'reusability',
     title: 'Reusability',
     subtitle: 'Effortless Code Reuse',
-    color: 'bg-green-400/[0.03]',
-    gradientColor: gradientSectionColors.green,
+    color: 'bg-[#94F99E]',
+    iconSrc: '/reusability-icon.svg',
     features: reusabilityFeatures,
   },
   {
     sectionName: 'scalability',
     title: 'Scalability',
     subtitle: 'Unleash Limitless Growth',
-    color: 'bg-yellow-400/[0.03]',
-    gradientColor: gradientSectionColors.yellow,
+    color: 'bg-[#FF7D7D]',
+    iconSrc: '/scalability-icon.svg',
     features: scalabilityFeatures,
   },
   {
     sectionName: 'tokenomics',
     title: 'Tokenomics',
     subtitle: 'Empowering Honest and Unbiased Voting',
-    color: 'bg-pink-400/[0.03]',
-    gradientColor: gradientSectionColors.pink,
+    color: 'bg-[#FFF58B]',
+    iconSrc: '/tokenomics-icon.svg',
     features: tokenomicsFeatures,
   },
   {
     sectionName: 'validators',
     title: 'Application Validators',
     subtitle: 'Rewarding Peak-Performing Modules',
-    color: 'bg-cyan-400/[0.03]',
-    gradientColor: gradientSectionColors.cyan,
+    color: 'bg-[#FFB5ED]',
+    iconSrc: '/validators-icon.svg',
     features: validatorsFeatures,
   },
 ]
