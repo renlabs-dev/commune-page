1d9b5f37bfe96cd74832b9f0fa3f238478e0fdca
Ed Castro <ed.sdr@outlook.com>
2024-03-26 13:02:37 -0300

    feat: docs colors for new layout

--- app/components/DocSidebar/doc-sidebar.component.tsx
+++ app/components/DocSidebar/doc-sidebar.component.tsx
@@ -20,7 +20,7 @@ export const DocSidebar = (props: TDocSidebarProps) => {
   const toggleMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen)
 
   const commonButtonClass =
-    'flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 text-white p-1.5 hover:bg-gray-100/[0.15]'
+    'flex h-12 w-12 items-center justify-center rounded-2xl bg-gray-100/10 text-title p-1.5 hover:bg-gray-100/[0.15]'
 
   const { params, activeTutorial, activeContent, prefix } = props
   return (
@@ -30,7 +30,7 @@ export const DocSidebar = (props: TDocSidebarProps) => {
         onClick={toggleMobileMenu}
         aria-label='Global'
       >
-        <div className='sm:min-w-2/6 relative h-full w-4/6 overflow-y-scroll border-r border-gray-50/[0.06] bg-gray-900 p-8 sm:w-3/6 md:w-2/6 lg:mx-auto lg:w-full'>
+        <div className='sm:min-w-2/6 relative h-full w-4/6 overflow-y-scroll border-r border-gray-50/[0.06] bg-white p-8 sm:w-3/6 md:w-2/6 lg:mx-auto lg:w-full'>
           <button
             type='button'
             className={`${commonButtonClass} absolute right-0 top-0 m-5 h-8 w-8 rounded-lg lg:hidden`}
@@ -42,20 +42,18 @@ export const DocSidebar = (props: TDocSidebarProps) => {
           {tutorials.map((tutorial, index) => {
             return (
               <div key={index}>
-                <span className='text-base font-medium text-white'>
-                  {tutorial.title}
-                </span>
+                <span className='text-base font-medium'>{tutorial.title}</span>
                 <div className='my-3 ml-1'>
                   {tutorial.contents.map((content, index) => {
                     return (
                       <Link
                         key={index}
                         href={`${prefix}/${tutorial.tutorialId}/${content.href}`}
-                        className={`relative mt-0 flex items-center border-l border-gray-600/70 p-3 ${params.slug[1] === content.href && params.slug[0] === tutorial.tutorialId ? 'text-gray-200' : 'text-gray-500  hover:text-gray-300'}`}
+                        className={`relative mt-0 flex items-center border-l border-gray-600/70 p-3 ${params.slug[1] === content.href && params.slug[0] === tutorial.tutorialId ? 'text-title' : 'text-gray-500  hover:text-gray-900'}`}
                       >
                         {params.slug[1] === content.href &&
                           params.slug[0] === tutorial.tutorialId && (
-                            <div className='absolute -left-1 h-2 w-2 rounded-full bg-gray-200' />
+                            <div className='absolute -left-1 h-2 w-2 rounded-full bg-red-500' />
                           )}
                         <span>{content.name}</span>
                       </Link>
@@ -80,7 +78,7 @@ export const DocSidebar = (props: TDocSidebarProps) => {
             {tutorials[activeTutorial].title}
           </span>
           <ChevronRightIcon width={16} />
-          <span className='ml-2 font-semibold text-white'>
+          <span className='ml-2 font-semibold text-black'>
             {tutorials[activeTutorial].contents[activeContent].name}
           </span>
         </div>
