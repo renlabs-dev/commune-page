0e18bcf02ae86debac081c0cec75d886c34ee6fa
Ed Castro <ed.sdr@outlook.com>
2024-03-26 13:16:52 -0300

    feature: new frequent questions section

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
           />
         )
       })}
-      {/* <FrequentQuestions /> */}
+      <FrequentQuestions />
       {/* <Footer /> */}
     </Suspense>
   )
