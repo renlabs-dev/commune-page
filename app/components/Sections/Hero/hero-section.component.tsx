d32facc36371b27f92a6d68f5e0c9488ca0e6d47
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-03-27 15:05:37 +1100

    feature: adjusting links on hero

--- app/components/Sections/Hero/hero-section.component.tsx
+++ app/components/Sections/Hero/hero-section.component.tsx
@@ -15,13 +15,13 @@ export const HeroSection = () => {
         alt={`Commune's logo`}
         className='w-2/3 max-w-sm pb-10 lg:mr-28 lg:w-auto lg:max-w-none lg:pb-0'
       />
-      <div className='flex h-full w-full flex-col items-center justify-center lg:w-auto lg:items-start'>
+      <div className='flex flex-col items-center justify-center w-full h-full lg:w-auto lg:items-start'>
         <h1
           className={`${cairo.className} pb-16 text-center text-5xl font-bold text-title lg:pb-0 lg:text-left lg:text-6xl`}
         >
           Commune Ai
         </h1>
-        <h2 className='text-center text-xl font-medium text-subtitle lg:text-left'>
+        <h2 className='text-xl font-medium text-center text-subtitle lg:text-left'>
           Renovating the way we build software for{' '}
           <span
             className={`${covered_by_your_grace.className} text-2xl font-normal text-[#62ABFF]`}
@@ -31,7 +31,7 @@ export const HeroSection = () => {
           .
         </h2>
         <Link
-          href='#discord'
+          href='#welcome'
           className='mt-8 w-full max-w-screen-sm rounded-xl border-2 border-title bg-[#FF6C6C] px-12 py-6 text-center text-sm font-bold text-title shadow-custom lg:w-auto'
         >
           <span aria-label='Get started with Commune AI'>GET STARTED NOW</span>
