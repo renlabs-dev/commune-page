3be902a54bfacf7faaa7d049dc3afaec8241d5e0
Ed Castro <ed.sdr@outlook.com>
2024-03-27 19:50:51 -0300

    fix: remove unnecessary prop

--- app/components/Sections/GenericSection/generic-section.component.tsx
+++ app/components/Sections/GenericSection/generic-section.component.tsx
@@ -12,7 +12,6 @@ type GenericSectionProps = {
   sectionName: string
   features: Feature[]
   lightColor: string
-  darkColor: string
   bgColor: string
   index: number
   color: string
@@ -25,7 +24,6 @@ export const GenericSection = ({
   sectionName,
   features,
   lightColor,
-  darkColor,
   bgColor,
   index,
   color,
@@ -34,10 +32,10 @@ export const GenericSection = ({
   return (
     <section
       id={sectionName}
-      className={`${index % 2 === 1 ? `dark:${darkColor} ${lightColor}` : 'dark:bg-[#131B2A]'} flex h-full w-full items-center justify-center p-4 lg:p-6`}
+      className={`${index % 2 === 1 ? `dark:bg-[#0C1320] ${lightColor}` : 'dark:bg-[#131B2A]'} flex h-full w-full items-center justify-center p-4 lg:p-6`}
     >
       <div
-        className={`w-full max-w-screen-sm rounded-xl bg-white lg:max-w-screen-xl dark:bg-[#131B2A] ${index % 2 === 1 ? "border-2 border-title bg-[url('/section-background-squares.svg')] bg-cover shadow-custom" : "bg-[url('/section-background-dots.svg')] bg-cover"} p-5 lg:px-16 lg:py-16`}
+        className={`w-full max-w-screen-sm rounded-xl bg-white dark:bg-[#131B2A] lg:max-w-screen-xl ${index % 2 === 1 ? "border-2 border-title bg-[url('/section-background-squares.svg')] bg-cover shadow-custom" : "bg-[url('/section-background-dots.svg')] bg-cover"} p-5 lg:px-16 lg:py-16`}
       >
         <div className='relative flex flex-col bg-clip-border text-left'>
           <div className='flex items-center'>
@@ -47,11 +45,11 @@ export const GenericSection = ({
                 width={70}
                 height={70}
                 alt={sectionName}
-                className='hover:animate-tada min-h-[80px] min-w-[80px] lg:h-[110px] lg:w-[110px]'
+                className='min-h-[80px] min-w-[80px] hover:animate-tada lg:h-[110px] lg:w-[110px]'
               />
             </div>
             <div className='flex flex-col justify-center'>
-              <p className='mt-2 text-left text-2xl font-bold tracking-tight text-title sm:text-4xl lg:text-5xl dark:text-white'>
+              <p className='mt-2 text-left text-2xl font-bold tracking-tight text-title dark:text-white sm:text-4xl lg:text-5xl'>
                 {title}
               </p>
               <h2
@@ -61,7 +59,7 @@ export const GenericSection = ({
               </h2>
             </div>
           </div>
-          <dl className='mx-auto mt-16 grid grid-cols-1 gap-8 text-base leading-7 text-subtitle sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16 dark:text-gray-200'>
+          <dl className='mx-auto mt-16 grid grid-cols-1 gap-8 text-base leading-7 text-subtitle dark:text-gray-200 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16'>
             {features.map((feature, index) => (
               <div key={index} className='relative pl-9'>
                 <dt className='inline font-semibold text-white'>
