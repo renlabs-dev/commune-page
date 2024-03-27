d76cabd64ad44892899310d67b08839e369f57b1
Vinícius Porto do Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-03-27 15:08:09 -0300

    fix: section content (#49)

--- app/utils/sections-mock/networking-features.ts
+++ app/utils/sections-mock/networking-features.ts
@@ -2,8 +2,6 @@ import {
   ExclamationTriangleIcon,
   BuildingOffice2Icon,
   CodeBracketSquareIcon,
-  ShareIcon,
-  GlobeAltIcon,
 } from '@heroicons/react/20/solid'
 
 export const networkingFeatures = [
@@ -14,22 +12,12 @@ export const networkingFeatures = [
   },
   {
     description:
-      'Developers face challenges in integrating and working across different tools, leading to inefficiencies and time wastage.',
+      'Developers face challenges in integrating and working across different tools, leading to inefficiencies and wasted time.',
     icon: CodeBracketSquareIcon,
   },
   {
     description:
-      'Lack of interoperability limits innovation and collaboration within the development community.',
-    icon: ExclamationTriangleIcon,
-  },
-  {
-    description:
       'Commune is a revolutionary protocol that connects developer tools, fostering collaboration, interoperability, and innovation within the development community.',
-    icon: ShareIcon,
-  },
-  {
-    description:
-      'Designed to break down barriers and unlock the potential of shared resources, Commune revolutionizes the way developers work and collaborate.',
-    icon: GlobeAltIcon,
+    icon: ExclamationTriangleIcon,
   },
 ]
--- app/utils/sections-mock/validators-features.ts
+++ app/utils/sections-mock/validators-features.ts
@@ -1,29 +1,17 @@
 import {
-  ArrowPathIcon,
   CloudArrowUpIcon,
-  FingerPrintIcon,
   LockClosedIcon,
 } from '@heroicons/react/20/solid'
 
 export const validatorsFeatures = [
   {
     description:
-      'Application validators validate modules that best performs that application.',
+      `Validators have to evaluate miners at regular intervals, ensuring coverage of the entire network and updating for new projects.`,
     icon: CloudArrowUpIcon,
   },
   {
     description:
-      'Each validator has its own objective and is responsible for calculating an appropriate reward it can vote based on.',
+      `Miner's prioritize serving Validators based on Stake and consensus, the more Stake the more weight on incentives.`,
     icon: LockClosedIcon,
-  },
-  {
-    description:
-      'Validators will need to stake to vote, and the higher staked validators will have more rewards from hosting problems.',
-    icon: ArrowPathIcon,
-  },
-  {
-    description:
-      'Modules are rewarded based on their performance with the Validator.',
-    icon: FingerPrintIcon,
   },
 ]
