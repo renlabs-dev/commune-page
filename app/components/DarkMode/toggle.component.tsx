e676da75d875f97f01122349703e1d08d73ca735
Vinicius Sacramento <50849605+vinicius-sacramento@users.noreply.github.com>
2024-04-20 03:02:49 -0300

    feature: enhance theme toggle (#78)
    * fix: white background and toggle behaviour on dark theme
    
    * fix: toggle changing theme when closed
    
    * fix: responsivity bg button
    
    * fix: toggle doesnt show right button on dark mode

--- app/components/DarkMode/toggle.component.tsx
+++ app/components/DarkMode/toggle.component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
   DARK_THEME_CLASSNAME,
@@ -33,9 +33,8 @@ const ThemeOption = ({
 }: ThemeOptionProps) => {
   return (
     <div
-      className={`${
-        currentTheme === theme ? 'bg-gray-100 dark:bg-[#131B2A]' : ''
-      } flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-title hover:bg-gray-200 hover:text-gray-900 dark:text-titleDark dark:hover:bg-blue-900`}
+      className={`${currentTheme === theme ? 'bg-gray-100 dark:bg-[#131B2A]' : ''
+        } flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-title hover:bg-gray-200 last:rounded-b-lg first:rounded-t-lg hover:text-gray-900 dark:text-titleDark dark:hover:bg-blue-900`}
       onClick={() => handleThemeChange(theme)}
     >
       <span>{icon}</span>
@@ -46,26 +45,31 @@ const ThemeOption = ({
 
 export const DarkModeToggle = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-  const defaultTheme = useMemo(getStoredTheme, [])
-  const [currentTheme, setCurrentTheme] = useState<TThemes>(defaultTheme)
-
-  const isDarkTheme = currentTheme === DARK_THEME_CLASSNAME
-  const isSystemTheme = currentTheme === SYSTEM_THEME_CLASSNAME
-
-  const Icon = useMemo(() => {
-    const shouldUseSystemDarkTheme = isSystemTheme && isDarkSystemTheme()
-
-    if (isDarkTheme || shouldUseSystemDarkTheme) {
-      return <MoonIcon className={'h-6 dark:fill-title md:dark:fill-white'} />
-    }
-
-    return <SunIcon className={'h-6 dark:fill-title md:dark:fill-white'} />
-  }, [isSystemTheme, isDarkTheme])
+  const [currentTheme, setCurrentTheme] = useState<TThemes>(getStoredTheme())
+  const [icon, setIcon] = useState<React.ReactNode>(null)
 
   const handleThemeChange = (theme: TThemes) => {
     setCurrentTheme(theme)
     setIsDropdownOpen(false)
   }
+
+  useEffect(() => {
+    const handleIcon = () => {
+      const isDarkTheme = currentTheme === DARK_THEME_CLASSNAME
+      const isSystemTheme = currentTheme === SYSTEM_THEME_CLASSNAME
+
+      const shouldUseSystemDarkTheme = isSystemTheme && isDarkSystemTheme()
+
+      if (isDarkTheme || shouldUseSystemDarkTheme) {
+        return <MoonIcon className={`h-6 dark:fill-title md:dark:fill-white`} />
+      }
+
+      return <SunIcon className={'h-6 dark:fill-title md:dark:fill-white'} />
+    }
+
+    setIcon(handleIcon())
+  }, [currentTheme])
+
 
   useEffect(() => {
     setTheme(currentTheme)
@@ -93,16 +97,16 @@ export const DarkModeToggle = () => {
     <>
       <button
         type='button'
-        className='inline-flex items-center justify-center rounded-xl p-1 hover:bg-gray-100 focus:ring-1 focus:ring-gray-200 focus:ring-offset-2 dark:fill-title dark:ring-offset-transparent dark:hover:bg-[#0C1320] dark:focus:ring-titleDark'
+        className='inline-flex items-center justify-center rounded-xl p-1 hover:bg-gray-100 focus:ring-1 focus:ring-gray-200 focus:ring-offset-2 dark:fill-title dark:ring-offset-transparent md:dark:hover:bg-[#0C1320] dark:focus:ring-titleDark'
         onClick={() => setIsDropdownOpen(!isDropdownOpen)}
       >
-        {Icon}
+        {icon}
       </button>
       {isDropdownOpen && (
         <div className='dark:shadow-custom-dark absolute right-0 top-10 w-fit origin-top-right rounded-xl border-2 border-black bg-white shadow-custom dark:border-titleDark dark:bg-[#0C1320] md:right-0 md:top-12 md:mt-4'>
           <div
             role='menu'
-            className='py-1'
+            className='py-0.5'
             aria-orientation='vertical'
             aria-labelledby='options-menu'
           >
