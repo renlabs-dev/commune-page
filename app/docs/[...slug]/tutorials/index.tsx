1a5a5d73afbbac2f385ae25dd273aac92369d61b
Ed Castro <ed.sdr@outlook.com>
2024-04-25 12:40:31 -0300

    feat: docs new structure

--- app/docs/[...slug]/tutorials/index.tsx
+++ app/docs/[...slug]/tutorials/index.tsx
@@ -1,6 +1,41 @@
+// Getting Started
 import Intro from './intro.mdx'
+
+// Concepts
+import Basics from './basics.mdx'
+import WeightSystem from './weight-system.mdx'
+import Governance from './governance.mdx'
+
+// Installation
 import SetupCommune from './setup-commune.mdx'
-import CreateCommune from './create-commune.mdx'
+import SetupWallet from './setup-wallet.mdx'
+
+// Modules
+import WhatIsModule from './what-is-a-module.mdx'
+import ModuleRequirments from './module-requirments.mdx'
+import ModuleBasics from './module-basics.mdx'
+import ModuleCreate from './module-create.mdx'
+import ModuleDeploy from './module-deploy.mdx'
+import ModuleRegister from './module-register.mdx'
+import ModuleConnect from './module-connect.mdx'
+
+// Working with Keys
+import KeyBasics from './key-basics.mdx'
+
+// Mining and Validating
+import WhatIsMining from './what-is-mining.mdx'
+import WhatIsValidating from './what-is-validating.mdx'
+import ParticipatingOnSubnet from './participating-on-a-subnet.mdx'
+import RunMiner from './run-miner.mdx'
+import RunValidator from './run-validator.mdx'
+import CreateValidator from './create-validator.mdx'
+import CreateMiner from './create-miner.mdx'
+
+// Subspace
+import SubspaceNodeList from './subspace-node-list.mdx'
+import RegistrationBurnMechanism from './registration-burn-mechanism.mdx'
+import GlobalParameters from './global-parameters.mdx'
+import RunningLocalNode from './running-local-node.mdx'
 
 const gettingStartedSection = [
   {
@@ -8,18 +43,186 @@ const gettingStartedSection = [
     href: `intro`,
     name: 'Introduction',
   },
+]
+
+const conceptsSection = [
+  {
+    component: <Basics />,
+    href: `basics`,
+    name: 'Basics',
+  },
+  {
+    component: <WeightSystem />,
+    href: `weight-system`,
+    name: 'Weight System',
+  },
+  {
+    component: <Governance />,
+    href: `governance`,
+    name: 'Governance',
+  },
+]
+
+const installationSection = [
   {
     component: <SetupCommune />,
     href: `setup-commune`,
     name: 'Setup Commune',
   },
+  {
+    component: <SetupWallet />,
+    href: `setup-wallet`,
+    name: 'Setup Wallet',
+  },
 ]
 
-const basicsSection = [
+const modulesSection = [
   {
-    component: <CreateCommune />,
-    href: `create-commune`,
-    name: 'Create Commune',
+    component: <WhatIsModule />,
+    href: `what-is-a-module`,
+    name: 'What is a module',
+  },
+  {
+    component: <ModuleRequirments />,
+    href: `module-requirments`,
+    name: 'Module Requirements',
+  },
+  {
+    component: <ModuleBasics />,
+    href: `module-basics`,
+    name: 'Module Basics ',
+  },
+  {
+    component: <ModuleCreate />,
+    href: `module-create`,
+    name: 'Create a Module',
+  },
+  {
+    component: <ModuleDeploy />,
+    href: `module-deploy`,
+    name: 'Deploy a Module',
+  },
+  {
+    component: <ModuleRegister />,
+    href: `module-register`,
+    name: 'Register a Module',
+  },
+  {
+    component: <ModuleConnect />,
+    href: `module-connect`,
+    name: 'Connect to a Module',
+  },
+]
+
+const workingWithKeysSection = [
+  {
+    component: <KeyBasics />,
+    href: `key-basics`,
+    name: 'Key Basics',
+  },
+]
+
+const miningAndValidatingSection = [
+  {
+    component: <WhatIsMining />,
+    href: `what-is-mining`,
+    name: 'What is mining',
+  },
+  {
+    component: <WhatIsValidating />,
+    href: `what-is-validating`,
+    name: 'What is validating',
+  },
+  {
+    component: <ParticipatingOnSubnet />,
+    href: `participating-on-a-subnet`,
+    name: 'Participating on a subnet as validator or miner',
+  },
+  {
+    component: <RunMiner />,
+    href: `run-miner`,
+    name: 'How to run a miner',
+  },
+  {
+    component: <RunValidator />,
+    href: `run-validator`,
+    name: 'How to run a validator',
+  },
+  {
+    component: <CreateValidator />,
+    href: `create-validator`,
+    name: 'How to Create and Deploy a Validator',
+  },
+  {
+    component: <CreateMiner />,
+    href: `create-miner`,
+    name: 'How to Create and Deploy a Miner',
+  },
+]
+
+// const subnetsSection = [
+//   {
+//     component: <SubnetBasics />,
+//     href: `subnet-basics`,
+//     name: 'Subnet Basics',
+//   },
+//   {
+//     component: <CreateSubnet />,
+//     href: `create-subnet`,
+//     name: 'Create a subnet',
+//   },
+//   {
+//     component: <DeploySubnet />,
+//     href: `deploy-subnet`,
+//     name: 'Deploy a subnet',
+//   },
+//   {
+//     component: <SubnetList />,
+//     href: `subnet-list`,
+//     name: 'Subnet List',
+//   },
+//   {
+//     component: <StakeThresholdCalcualtion />,
+//     href: `subnet-threshold-calculation`,
+//     name: 'Stake threshold calcualtion',
+//   },
+//   {
+//     component: <GeneralNetwork />,
+//     href: `general-network`,
+//     name: 'General Network',
+//   },
+//   {
+//     component: <GeneralNetworkGovernance />,
+//     href: `general-network-governance`,
+//     name: 'General network governance',
+//   },
+//   {
+//     component: <SubnetParameters />,
+//     href: `subnet-parameters`,
+//     name: 'Subnet parameters',
+//   },
+// ]
+
+const subspaceSection = [
+  {
+    component: <SubspaceNodeList />,
+    href: `subspace-node-list`,
+    name: 'Subspace Node List',
+  },
+  {
+    component: <RegistrationBurnMechanism />,
+    href: `registration-burn-mechanism`,
+    name: 'Registration Burn Mechanism',
+  },
+  {
+    component: <GlobalParameters />,
+    href: `global-parameters`,
+    name: 'Global Parameters',
+  },
+  {
+    component: <RunningLocalNode />,
+    href: `running-local-node`,
+    name: 'Running a Local Node',
   },
 ]
 
@@ -30,43 +233,38 @@ export const tutorials = [
     tutorialId: 'getting-started',
   },
   {
-    contents: basicsSection,
-    title: 'Basics',
-    tutorialId: 'basics',
+    contents: conceptsSection,
+    title: 'Concepts',
+    tutorialId: 'concepts',
+  },
+  {
+    contents: installationSection,
+    title: 'Installation',
+    tutorialId: 'installation',
+  },
+  {
+    contents: modulesSection,
+    title: 'Modules',
+    tutorialId: 'modules',
+  },
+  {
+    contents: workingWithKeysSection,
+    title: 'Working with Keys',
+    tutorialId: 'working-with-keys',
+  },
+  {
+    contents: miningAndValidatingSection,
+    title: 'Mining and Validating',
+    tutorialId: 'mining-and-validating',
   },
   // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
+  //   contents: subnetsSection,
+  //   title: 'Subnets',
+  //   tutorialId: 'subnets',
   // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
-  // {
-  //   contents: [...basicsSection, ...gettingStartedSection],
-  //   title: 'Mock',
-  //   tutorialId: 'mocked',
-  // },
+  {
+    contents: subspaceSection,
+    title: 'Subspace',
+    tutorialId: 'subspace',
+  },
 ]
