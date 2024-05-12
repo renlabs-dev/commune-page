a210cb19a1443ca1bcc859eeacc6014221b8c990
Ed Castro <ed.sdr@outlook.com>
2024-05-12 13:46:54 -0300

    fix: container width on desktop

--- app/components/Sections/Hero/hero-section.component.tsx
+++ app/components/Sections/Hero/hero-section.component.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { applicationsList } from '@/app/utils'
-//app/components/Animation/index.jsx
 import Animation from '@/app/components/Animation'
 
 const networkSpecs = [
@@ -52,52 +51,57 @@ export const HeroSection = async () => {
       id='hero'
       className='flex w-full flex-col justify-center overflow-hidden text-gray-400'
     >
-      <div className='flex min-h-[55dvh] flex-col-reverse lg:flex-row'>
+      <div className='flex flex-col-reverse lg:flex-row'>
         <div className='flex w-full flex-col justify-between border-white lg:max-w-[60%] lg:border-r'>
-          <div className='lg:py-50 flex h-full w-full flex-col justify-center space-y-4 border-b border-white px-4 py-20 lg:p-20'>
-            <p className='text-xl font-medium'>
-              <span className='text-green-400'>Peer-to-peer </span>
-              incentivized coordination network.
-            </p>
-            <Image
-              src='/logo-asci.svg'
-              width={200}
-              height={100}
-              alt='Commune ai logo'
-              className='w-full py-4'
-            />
-            <p className='font-normal'>
-              Commune AI is a peer-to-peer protocol with a stake-based market of
-              Modules, linking on-chain identifiers to off-chain computable
-              objects via APIs. This setup supports complex computations and
-              diverse applications, from AI models to storage solutions,
-              independent of the blockchain.
-            </p>
+          <div className='lg:py-50 flex h-full flex-col justify-center space-y-4 border-b border-white px-4 py-20 lg:p-20'>
+            <div className='flex w-full justify-end'>
+              <div className='flex w-full flex-col justify-between lg:max-w-4xl lg:pl-24'>
+                <p className='text-xl font-medium'>
+                  <span className='text-green-400'>Peer-to-peer </span>
+                  incentivized coordination network.
+                </p>
+                <Image
+                  src='/logo-asci.svg'
+                  width={200}
+                  height={100}
+                  alt='Commune ai logo'
+                  className='w-full py-4'
+                />
+                <p className='font-normal'>
+                  Commune AI is a peer-to-peer protocol with a stake-based
+                  market of Modules, linking on-chain identifiers to off-chain
+                  computable objects via APIs. This setup supports complex
+                  computations and diverse applications, from AI models to
+                  storage solutions, independent of the blockchain.
+                </p>
+              </div>
+            </div>
           </div>
-
-          <div className='flex w-full justify-between'>
-            {networkSpecs.map((spec, index) => {
-              return (
-                <div
-                  key={index}
-                  className='flex w-1/3 items-center justify-center border-x border-white py-8 first:border-none last:border-none lg:border-none'
-                >
-                  <div className='flex w-auto flex-col items-start justify-start'>
-                    <p className='text-2xl font-semibold text-white lg:text-3xl'>
-                      {spec.info}
-                      <span className='text-green-500'>+</span>
-                    </p>
-                    <p className='text-left text-sm font-normal'>
-                      {spec.title}
-                    </p>
+          <div className='flex w-full justify-end'>
+            <div className='flex w-full justify-between lg:max-w-5xl lg:pl-12'>
+              {networkSpecs.map((spec, index) => {
+                return (
+                  <div
+                    key={index}
+                    className='flex w-1/3 items-center justify-center border-x border-white py-8 first:border-none last:border-none lg:border-none'
+                  >
+                    <div className='flex w-auto flex-col items-start justify-start'>
+                      <p className='text-2xl font-semibold text-white lg:text-3xl'>
+                        {spec.info}
+                        <span className='text-green-500'>+</span>
+                      </p>
+                      <p className='text-left text-sm font-normal'>
+                        {spec.title}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
         </div>
         <div className='0 relative flex flex-col items-center justify-center border-b border-white p-4 lg:w-[45%] lg:flex-row lg:border-none lg:p-0'>
-          <div className='-z-50'>
+          <div className='-z-50 animate-fade-in-up'>
             <Animation />
           </div>
 
@@ -117,31 +121,33 @@ export const HeroSection = async () => {
         </div>
       </div>
 
-      <div className='flex flex-col justify-between border-t border-white lg:flex-row'>
-        {applicationsList.map((app, index) => {
-          return (
-            <Link
-              key={index}
-              href={app.href}
-              className='w-full border-b border-white p-10 px-4 last:border-none hover:bg-gray-200/5 hover:text-gray-300 lg:border-b-0 lg:border-l lg:border-r lg:p-16 lg:py-10 lg:first:border-none'
-            >
-              {app.icon}
-              <div id='welcome' className='flex justify-between'>
-                <div>
-                  <p className='text-white'>{app.title}</p>
-                  {handleDescription(app.description)}
+      <div className='flex justify-center border-t border-white'>
+        <div className='flex w-full max-w-screen-2xl flex-col lg:flex-row'>
+          {applicationsList.map((app, index) => {
+            return (
+              <Link
+                key={index}
+                href={app.href}
+                className='w-full border-b border-white p-10 px-4 last:border-none hover:bg-gray-200/5 hover:text-gray-300 lg:border-b-0 lg:border-l lg:border-r lg:p-16 lg:py-10 lg:first:border-none'
+              >
+                {app.icon}
+                <div id='welcome' className='flex justify-between'>
+                  <div>
+                    <p className='text-white'>{app.title}</p>
+                    {handleDescription(app.description)}
+                  </div>
+                  <Image
+                    src={'/arrow-link-icon.svg'}
+                    alt='link icon'
+                    width={75}
+                    height={75}
+                    className='w-12 border border-green-500 p-3'
+                  />
                 </div>
-                <Image
-                  src={'/arrow-link-icon.svg'}
-                  alt='link icon'
-                  width={75}
-                  height={75}
-                  className='w-12 border border-green-500 p-3'
-                />
-              </div>
-            </Link>
-          )
-        })}
+              </Link>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
