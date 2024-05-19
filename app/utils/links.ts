9128383607a0536c880c6e0846337f9afcb5cf62
Ed Castro <ed.sdr@outlook.com>
2024-05-19 17:33:32 -0300

    feat: wallet button on commune page

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -7,6 +7,7 @@ export const links = {
   x: 'https://twitter.com/communeaidotorg',
   telegram: 'https://t.me/communecommunity',
   governance: 'https://governance.communeai.org/',
+  wallet: 'https://wallet.communeai.org/',
   updates: '/',
   articles: 'https://mirror.xyz/0xD80E194aBe2d8084fAecCFfd72877e63F5822Fc5',
 }
